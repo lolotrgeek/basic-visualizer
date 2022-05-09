19876696201bd296e1a747073d3de7b14a9d47b3
lolotrgeek <jon@simplyenable.com>
2022-05-09 17:02:38 -0500

    feat: classify Particle, add, remove, update

--- src/client/index.js
+++ src/client/index.js
@@ -2,12 +2,13 @@ let div = document.getElementById("sketch")
 let x = div.offsetWidth
 let y = div.offsetHeight
 let scalar = 10
-let world = new World({},[])
+let world = new World({}, [])
 let cnv
 
-function startsketch(world) {
-    let sketch = function (p) {
+function startsketch() {
+    const sketch = p => {
         world.p = p
+        world.p.Vector = p5.Vector
         p.setup = function () {
             cnv = p.createCanvas(div.offsetWidth, div.offsetHeight)
             cnv.style('display', 'block')
@@ -21,41 +22,53 @@ function startsketch(world) {
         }
     }
 
-    let myp5 = new p5(sketch)
+    let p5instance = new p5(sketch)
 }
 function stopsketch(world) {
     console.log('stopping')
     world.p.remove()
 }
 
-startsketch(world)
+startsketch()
 
 listen(msg => {
     if (typeof msg === 'object') {
 
         if (typeof msg.world === 'object') {
             if (msg.world.id && msg.world.distances) {
-                let particles = Object.keys(msg.world.distances).map(key => {
-                    let particle = {}
-                    particle.id = key
-                    particle.size = 2
-                    particle.distance = msg.world.distances[key].distance
-                    particle.position = { x: msg.world.distances[key].distance, y: msg.world.distances[key].distance }
-                    return particle
-                })
+
                 let self = {}
                 self.id = msg.world.id
-                self.size = 10
-
-                console.log(particles)
+                self.size = 20
 
                 world.self = self
-                world.particles = particles
-            }
-            // node = circle
-            // distance = length of line + text
-            // click on node = show distances for that node -> redraw
+                world.center()
 
+                let particleMap = Object.keys(msg.world.distances).map(key => new Particle(world.p, self.position.x, self.position.y, msg.world.distances[key].distance, 10, key, []))
+                let particles = particleMap.map((particle, index) => {
+                    particle.others = particleMap.filter(other => other.id !== particle.id)
+                    // particle.others = particleMap
+                    return particle
+                })
+
+                // remove particles
+                world.particles.forEach((world_particle, inWorld) => {
+                    if (world_particle) {
+                        let outWorld = particles.findIndex(particle => world_particle.id === particle.id)
+                        if (outWorld === -1) remove_particle(inWorld)
+                    }
+                })
+
+                // update or add new particles
+                particles.forEach(particle => {
+                    if (particle) {
+                        let inWorld = world.particles.findIndex(world_particle => world_particle.id === particle.id)
+                        if (inWorld > -1) update_particle(particle, inWorld)
+                        else add_particle(particle)
+                    }
+                })
+
+            }
         }
 
     }
@@ -64,3 +77,14 @@ listen(msg => {
     }
 })
 
+function add_particle(particle) {
+    world.particles = [...world.particles, particle]
+    return
+}
+
+function update_particle(particle, index) {
+    world.particles[index].distance = particle.distance
+}
+function remove_particle(index) {
+    world.particles.splice(index, 1)
+}
--- src/client/world.js
+++ src/client/world.js
@@ -8,16 +8,16 @@ class World {
         this.particles = particles
         this.self = self
         this.p = null
-        this.scaler = 10
+        this.vec = null
     }
 
-    showdistance(position, objective, distance) {
+    showdistance(position, objective, text) {
         this.p.push()
-        this.p.fill(0)
+        this.p.fill('#FFF')
         this.p.line(position.x, position.y, objective.x, objective.y)
         this.p.translate((position.x + objective.x) / 2, (position.y + objective.y) / 2)
         this.p.rotate(this.p.atan2(objective.y - position.y, objective.x - position.x))
-        this.p.text(this.p.nfc(distance.toString(), 1), 0, -5)
+        this.p.text(this.p.nfc(text.toString(), 1), 0, -5)
         this.p.pop()
     }
 
@@ -32,28 +32,7 @@ class World {
         this.colorize(particle)
         this.p.ellipse(particle.position.x, particle.position.y, particle.size, particle.size)
     }
-
-    nearby(self, particles) {
-        if (particles.length > 0) {
-            particles.forEach(particle => {
-                this.showdistance(self.position, particle.position, particle.distance)
-            })
-        }
-    }
-
-    inside(particle, thingLocation) {
-        let distance = p5.Vector.dist(particle.position, thingLocation)
-        if (distance < particle.size) return true
-        else return false
-    }
-
-    position(particle) {
-        let x = this.self.position.x + (particle.position.x) * this.scaler
-        let y = this.self.position.y + (particle.position.y) * this.scaler
-        return this.p.createVector(x, y)
-    }
-
-    center(){
+    center() {
         this.self.position = {}
         this.self.position.x = this.p.width / 2
         this.self.position.y = this.p.height / 2
@@ -61,15 +40,73 @@ class World {
 
     spin() {
         this.center()
-        this.display(this.self)
+        if (this.self && this.self.position && typeof this.self.size === 'number') this.display(this.self)
         this.particles.forEach(particle => {
-            if(!particle.positioned) {
-                particle.position = this.position(particle)
-                particle.positioned = true
+            if (particle) {
+                this.colorize()
+                particle.separate()
+                particle.update()
+                if (particle.position && particle.position.x && particle.position.y && typeof particle.size === 'number' && particle.distance) {
+                    this.display(particle)
+                    this.showdistance(this.self.position, particle.position, particle.distance)
+                }
             }
-            this.display(particle)
-            this.showdistance(this.self.position, particle.position, particle.distance)
-        })
 
+        })
     }
+}
+
+class Particle {
+    constructor(p, x, y, distance, size, id, others) {
+        this.p = p
+        this.position = this.p.createVector(x, y)
+        this.distance = distance
+        this.size = size
+        this.id = id
+        this.others = others
+        this.maxspeed = 3// Maximum speed
+        this.maxforce = 0.2 // Maximum steering force
+        this.acceleration = this.p.createVector(0, 0)
+        this.velocity = this.p.createVector(0, 0)
+    }
+
+    separate(others) {
+        let desiredseparation = this.size * 2
+        let sum = this.p.createVector()
+        let count = 0
+        // For every boid in the system, check if it's too close
+        for (let i = 0; i < this.others.length; i++) {
+            let d = this.p.Vector.dist(this.position, this.others[i].position)
+            if (d < desiredseparation) {
+                // Calculate vector pointing away from neighbor
+                let diff = this.p.Vector.sub(this.position, this.others[i].position)
+                diff.normalize()
+                diff.div(d === 0 ? 1: d) // Weight by distance
+                sum.add(diff)
+                count++ // Keep track of how many
+            }
+        }
+        // Average -- divide by how many
+        if (count > 0) {
+            sum.div(count)
+            // Our desired vector is the average scaled to maximum speed
+            sum.normalize()
+            sum.mult(this.maxspeed)
+            // Implement Reynolds: Steering = Desired - Velocity
+            let steer = this.p.Vector.sub(sum, this.velocity)
+            steer.limit(this.maxforce)
+            this.acceleration.add(steer)
+        }
+    }
+
+    update() {
+        // Update velocity
+        this.velocity.add(this.acceleration)
+        // Limit speed
+        this.velocity.limit(this.maxspeed)
+        this.position.add(this.velocity)
+        // Reset accelertion to 0 each cycle
+        this.acceleration.mult(0)
+    }
+
 }
